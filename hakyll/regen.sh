fd769c35e79877dee1f7ce455de655a932fdcff2
Doug Beardsley <mightybyte@gmail.com>
2020-07-11 09:44:46 -0400

    Update regen script

--- hakyll/regen.sh
+++ hakyll/regen.sh
@@ -3,6 +3,6 @@
 SITE_DIR=../result/bin
 $SITE_DIR/site clean
 $SITE_DIR/site build
-rm ../index.html
-rm -fr ../pages
-cp -R _site/* ..
+rm ../docs/index.html
+rm -fr ../docs/pages
+cp -R _site/* ../docs
